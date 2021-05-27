a8eeaeda35d6844afc313e8c863ba21efb72f08a
Andrius <alimasWebDeveloper@gmail.com>
2021-05-27 18:05:23 +0100

    Update myscript.js
    Update myscript.js.
    
    Get modal object, button object and span close object for modal. Then we set that when we click start button we display this modal and we can close using span.onclick which set modal display to none.

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,22 @@
 (function () {
   emailjs.init("user_cqlFQGPQ83gcI8oKnBtvB"); // initialize emailjs
 })();
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("start");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
